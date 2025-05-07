cbfe8cbdfdef99c86d97a0f9ac598786382e8f48
IDGS-904-22001661 <armandodanielr29@outlook.com>
2025-05-06 19:36:36 -0600

    Ultimo tema: if-when

--- app/src/main/java/com/lifethech/introkotlin904/TemasKotlin/tema1Variables.kt
+++ app/src/main/java/com/lifethech/introkotlin904/TemasKotlin/tema1Variables.kt
@@ -1,38 +1,36 @@
 package com.lifethech.introkotlin904.TemasKotlin
 
 fun main(){
-    // variables solo lectura
-    //variables mutables var
+    //variables solo lectura val
+    val a=4
+    //variables mutablles var
 
-    val a = 4
-    val  b = 8
-    val c = 10
+
+    var b=8
+    var c=10
+
+
 
     println(a)
     println("el valor de b es $b")
     println("el valor de c + 2 es ${c+2}")
-/*
-    c=a+2
-    c+=6
+
+    c= a+2
+    c+=8
     c-=5
     c*8
     c/=2
- */
-
     println("el valor de c es $c")
+    /*
+    val num1:int=23
+    var num2:int=12
 
-    val num1: int = 23
-    var num2: int=12
+    num2=6
 
-    num2 = 6
-
-    var nombre:String="Armando"
+    var  nombre:String="carlos"
     var caracter:Char='a'
 
     var num4:Float=12.5f
     var num5:Double=12.5
-
-
-
-
+*/
 }
